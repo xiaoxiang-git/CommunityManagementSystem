4c0aa4932acc1aee25bd88efecbe12bf9253bf43
heyh8 <heyh8@asiainfo.com>
2020-09-02 17:22:20 +0800

    【修改】用户业务逻辑层实现类

--- src/main/java/com/springboot/cms/run/SpringBootApplication.java
+++ src/main/java/com/springboot/cms/run/SpringBootApplication.java
@@ -3,6 +3,7 @@ package com.springboot.cms.run;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 /**
  * SpringBoot启动类
@@ -12,6 +13,7 @@ import org.springframework.context.annotation.ComponentScan;
 @org.springframework.boot.autoconfigure.SpringBootApplication
 @ComponentScan("com.springboot.cms")
 @MapperScan("com.springboot.cms.dao")
+@EnableTransactionManagement
 public class SpringBootApplication {
 
     public static void main(String[] args) {
--- src/main/java/com/springboot/cms/service/implement/UserServiceImplement.java
+++ src/main/java/com/springboot/cms/service/implement/UserServiceImplement.java
@@ -4,41 +4,53 @@ import com.springboot.cms.dao.InterfaceUserDao;
 import com.springboot.cms.pojo.User;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
-import javax.annotation.Resource;
 import java.util.List;
 
-@Service(value="userServiceImplement")
+/**
+ * 用户业务逻辑层实现类
+ *
+ * @author YuanhuiHo
+ */
+@Service("userServiceImplement")
+@Transactional(rollbackFor = Exception.class)
 public class UserServiceImplement extends CommonServiceImplement<User> {
 
-    @Resource(name="interfaceUserDao")
+    @Autowired
     private InterfaceUserDao interfaceUserDao;
 
+    //保存用户
     @Override
     public void save(User user) {
         interfaceUserDao.save(user);
     }
 
+    //更新用户
     @Override
     public void update(User user) {
         interfaceUserDao.update(user);
     }
 
+    //删除用户
     @Override
     public void delete(Integer id) {
         interfaceUserDao.delete(id);
     }
 
+    //根据用户ID查找用户
     @Override
     public User findById(Integer id) {
         return interfaceUserDao.findById(id);
     }
 
+    //根据条件查找用户
     @Override
     public List<User> findByConditions(String... conditions) {
         return interfaceUserDao.findByConditions(conditions);
     }
 
+    //查找所有用户
     @Override
     public List<User> findAll() {
         return interfaceUserDao.findAll();
